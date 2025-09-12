23e3a8e8c67c9b839c5acc21d13b90f2e0da5cdf
Asif Chowdhury <47955356+AsifAC@users.noreply.github.com>
2025-09-11 23:03:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array called tasks.
+// TODO: Call the render function to update the table with the new tasks.
+
+// Section 2: App State Variables
+let tasks = [];
+
+// Section 3: DOM Elements
+const taskForm = document.getElementById("taskForm");
+const taskTable = document.getElementById("taskTable");
+
+// Test references
+console.log("taskForm:", taskForm);
+console.log("taskTable:", taskTable);
